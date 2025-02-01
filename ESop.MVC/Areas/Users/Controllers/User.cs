fbe579a225be0062971ac1c125aa1a93cf85c0a4
Matin-Rmz <MatinRmz82@gmail.com>
2025-02-02 00:46:32 +0330

    User Area Added, Home Index and Layout modified.

---
+++ ESop.MVC/Areas/Users/Controllers/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Areas.Users.Controllers
+{
+    public class User : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
